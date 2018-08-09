f071b1a47cbfcfa64fec93fbeb8aab78c4601088
Hassan Ali <hassan@appslab.io>
2018-08-09 09:59:22 +0500

    Login Page Complete. Capture Image working with GeoLocation and Map integration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
+
+import {
+  GoogleMaps
+} from '@ionic-native/google-maps';
+import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderForwardResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder';
+import { Diagnostic } from '@ionic-native/diagnostic';
+import { DatePicker } from '@ionic-native/date-picker';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,
+      {
+        mode:'ios'
+      })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
   ],
   providers: [
     StatusBar,
+    DatePicker,
     SplashScreen,
+    AndroidFingerprintAuth,
+    Camera,
+    Geolocation,
+    GoogleMaps,
+    NativeGeocoder,
+    BackgroundGeolocation,
+    Diagnostic,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
